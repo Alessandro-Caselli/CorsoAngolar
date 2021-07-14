82a3004a29fb69367cbfc79d81106360b07dc65a
Alessandro-Caselli <case94aksv@gmail.com>
2021-07-14 13:54:52 +0200

    password finito

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -1,3 +1,4 @@
+import { PasswordValidators } from './password.validators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,10 +12,21 @@ export class PasswordComponent {
 
   constructor(fb: FormBuilder) {
     this.form = fb.group({
-      oldPassword: ['', Validators.required],
-      newPassword: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
-    })
+      oldPassword: ['',
+        Validators.required,
+        PasswordValidators.validOldPassword
+      ],
+      newPassword: ['',
+        Validators.required
+      ],
+      confirmPassword: ['',
+        Validators.required]
+    }, {
+      validator: PasswordValidators.passwordsShouldMatch
+    });
   }
 
+  get oldPassword() { return this.form.get('oldPassword');}
+  get newPassword() { return this.form.get('newPassword');}
+  get confirmPassword() { return this.form.get('confirmPassword');}
 }
